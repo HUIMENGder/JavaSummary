06b985a99f73d859b1bc755d3b2a39ef679526e2
HUIMENG <761473149@qq.com>
2021-06-05 23:09:32 +0800

    删除

--- QiangZhiZhuanHuan.java
+++
@@ -1,7 +0,0 @@
-public class QiangZhiZhuanHuan{
-    public static void main(String[] args){
-        int i1 = 123;
-        byte b = (byte)i1;//强制类型转换为byte
-        System.out.println("int强制类型转换为byte后的值等于"+b);
-    }
-}
